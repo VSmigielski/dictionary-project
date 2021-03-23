f5a295c3e4955e422d527ad68fd883775567bff6
Veronica <veronicasmigielski@gmail.com>
2021-03-23 20:57:38 +0100

    Beginnings of Dictionary React Application

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import logo from './logo.png';
 import './App.css';
+import Dictionary from "./Dictionary.js";
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
         <img src={logo} className="img-fluid App-logo" alt="logo" />
-        
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer">
+        <small>Coded by Veronica Smigielski</small>
+      </footer>
+      </div>
     </div>
   );
 }
