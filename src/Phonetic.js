9e8e176eee77028cc31e98b956b671dbe4449255
Veronica <veronicasmigielski@gmail.com>
2021-03-24 04:11:10 +0100

    Updated the listen&text to use a grid

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -4,7 +4,7 @@ import "./Phonetic.css"
 export default function Phonetic(props) {
     if (props.phonetic) {
         return (
-            <div className="Phonetic">
+            <div className="Phonetic d-inline">
                 <a className="btn btn-secondary" rel="noreferrer" href={props.phonetic.audio} target="_blank">
                     Listen
                 </a>
--- src/Results.js
+++ src/Results.js
@@ -8,7 +8,11 @@ export default function Results(props) {
         return (
             <div className="Results mt-3">
                 <section>
+                <div className="row">
+                <div className="col-sm-6">
                 <h2>{props.results.word}</h2>
+                </div>
+                <div className="col-sm-6">
                 {props.results.phonetics.map(function(phonetic, index) {
                     return (
                         <div key={index}>
@@ -16,6 +20,8 @@ export default function Results(props) {
                         </div>
                     );
                 })}
+                </div>
+                </div>
                 </section>
                 {props.results.meanings.map(function(meaning, index) {
 
