dbefa71043b2406a42e8198a7d5e2abe245a95f2
Veronica <veronicasmigielski@gmail.com>
2021-03-24 01:01:13 +0100

    Used api call to send to two other components for use

---
+++ src/Meaning.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function Meaning(props) {
+    console.log(props.meaning);
+ return (
+ <div className="Meaning">
+     <div className="card mt-3">
+         <div className="card-body">
+    <h3>{props.meaning.partOfSpeech}</h3>
+    {props.meaning.definitions.map(function(definition, index) 
+    {
+        return (
+            <div key={index}>
+                <p>
+                {definition.definition}
+                <br />
+                <em>{definition.example}</em>
+                </p>
+                </div>     
+        );
+    })}
+    </div>
+    </div>
+ </div>
+ );
+}
